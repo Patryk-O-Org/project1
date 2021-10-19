731379e0aeb457fdd1dd40afda6cf686254cf255
POleskiewicz <poleskiewicz@hawk.iit.edu>
2021-10-19 16:39:16 -0500

    Updates
    updated layout, style, js
    
    colors
    text align

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@ document.getElementById("submit").addEventListener("click", function(evt) {
     section = document.getElementById("main");
     section.innerHTML = "";
 	confirmationNode = document.createElement("h3");
-    confirmationNode.innerHTML = "Keep a look out for emails about future promotions and events!"
     section.appendChild(acknowledgeNode);
     section.appendChild(confirmationNode);
     acknowledgeNode = document.createElement("h2");
-    acknowledgeNode.innerHTML = "Thank you for applying to the Nintendo Newsletter Service!";
+    
     
   }
  };
@@ -17,4 +16,4 @@ document.getElementById("submit").addEventListener("click", function(evt) {
 
 
  
- }
+ 
